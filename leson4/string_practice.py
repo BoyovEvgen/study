2eb4b18d1fe3471625762cbe9904f3fea3784329
BoyovEvgen <e.boyov@gmail.com>
2022-06-05 15:17:45 +0300

    string_practice

--- leson4/string_practice.py
+++ leson4/string_practice.py
@@ -27,3 +27,15 @@ string = 'Lorem, Ipsum, is, simply, dummy, text, of, the, printing, industry.'
     Результат: 'Lorem Ipsum is simply dummy tLorem Ipsum is simply dummy tLorem Ipsum is simply dummy text of the 
     printing industry.'
 """
+#1
+string = string.replace(',', '')
+print(string)
+#2
+print(string.rfind('s'))
+#3
+print(string.lower().count('i'))
+#4
+print(string[string.find('simply'):string.rfind('text')+4])
+#5
+string = string[:len(string)//2] * 3 + string[len(string)//2:]
+print(string)
